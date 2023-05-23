7e7aa92207dfb0aeb732ef30ace8036865e965a4
Mitch Ryan <mitch@mitch-ryan.com>
2023-05-23 19:14:07 +0200

    Make the tray menu dynamically updated with a click

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,48 @@
 import { app, Menu } from "electron";
 import { nativeImage } from "electron/common";
-import { Tray } from "electron/main";
+import { MenuItem, Tray } from "electron/main";
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
 
-app.whenReady().then(() => {
-  let icon = nativeImage.createFromPath(
-    "/Users/mitch/Desktop/Screenshot 2023-05-23 at 16.06.35.png"
-  );
-  icon = icon.resize({
-    height: 16,
-    width: 16,
-  });
-  const tray = new Tray(icon);
+app
+  .whenReady()
+  .then(() => {
+    let icon = nativeImage.createFromPath(
+      "/Users/mitch/Desktop/Screenshot 2023-05-23 at 16.06.35.png"
+    );
+    icon = icon.resize({
+      height: 16,
+      width: 16,
+    });
+    const tray = new Tray(icon);
 
-  const contextMenu = Menu.buildFromTemplate([
-    { label: `Last watered: ${new Date()}`, type: "normal", enabled: false },
-    { label: "I watered my plants today", type: "separator" },
-    { label: "I watered my plants on:", type: "normal" },
-  ]);
+    let contextMenu: Menu;
 
-  // Make a change to the context menu
-  contextMenu.items[1].checked = false;
+    const printLastWateredLabel = (date: Date) => `Last watered: ${date}`;
 
-  // Call this again for Linux because we modified the context menu
-  tray.setContextMenu(contextMenu);
-});
+    const lastWateredMenuItem = new MenuItem({
+      label: printLastWateredLabel(new Date()),
+      type: "normal",
+      enabled: false,
+    });
+
+    const wateredButton = new MenuItem({
+      label: "I watered my plants today",
+      type: "normal",
+      click: () => {
+        lastWateredMenuItem.label = "Updated";
+        build();
+      },
+    });
+
+    const menuTemplate = [lastWateredMenuItem, wateredButton];
+
+    const build = () => {
+      contextMenu = Menu.buildFromTemplate(menuTemplate);
+      tray.setContextMenu(contextMenu);
+    };
+
+    build();
+  })
+  .catch(console.error);

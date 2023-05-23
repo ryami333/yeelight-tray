c6831fb2c7ad204a33555da3c05e12e0070718f0
Mitch Ryan <mitch@mitch-ryan.com>
2023-05-23 19:53:10 +0200

    Sync the db.json file when there are changes to the db object

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { app, Menu } from "electron";
 import { nativeImage } from "electron/common";
 import { MenuItem, Tray } from "electron/main";
+import { z } from "zod";
 import { resolve } from "path";
-import { writeFileSync } from "fs";
+import { writeFileSync, readFileSync, existsSync } from "fs";
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
@@ -13,26 +14,45 @@ interface DB {
   lastWatered: Date;
 }
 
+const safeJsonParse = (source: string) => {
+  try {
+    return JSON.parse(source);
+  } catch {
+    return undefined;
+  }
+};
+
+if (!existsSync(DB_PATH)) {
+  writeFileSync(DB_PATH, "");
+}
+
 app
   .whenReady()
   .then(() => {
     const sync = () => {
       writeFileSync(DB_PATH, JSON.stringify(db));
+      build();
     };
 
-    const db = new Proxy(
-      {
-      lastWatered: new Date(),
-      } satisfies DB,
-      {
-        set(...args) {
-          const result = Reflect.set(...args);
-          console.log("TODO: sync");
-          sync();
-          return result;
-        },
-      }
-    );
+    const initialState: DB = z
+      .object({
+        lastWatered: z
+          .string()
+          .datetime()
+          .transform((str) => new Date(str)),
+      })
+      .catch({
+        lastWatered: new Date(),
+      })
+      .parse(safeJsonParse(readFileSync(DB_PATH, "utf8")));
+
+    const db = new Proxy(initialState, {
+      set(...args) {
+        const result = Reflect.set(...args);
+        sync();
+        return result;
+      },
+    });
 
     let icon = nativeImage.createFromPath(
       "/Users/mitch/Desktop/Screenshot 2023-05-23 at 16.06.35.png"
@@ -48,22 +68,21 @@ app
     const printLastWateredLabel = (date: Date) => `Last watered: ${date}`;
 
     const build = () => {
-    const lastWateredMenuItem = new MenuItem({
+      const lastWateredMenuItem = new MenuItem({
         label: printLastWateredLabel(new Date(db.lastWatered)),
-      type: "normal",
-      enabled: false,
-    });
+        type: "normal",
+        enabled: false,
+      });
 
-    const wateredButton = new MenuItem({
-      label: "I watered my plants today",
-      type: "normal",
-      click: () => {
+      const wateredButton = new MenuItem({
+        label: "I watered my plants today",
+        type: "normal",
+        click: () => {
           db.lastWatered = new Date();
-        build();
-      },
-    });
+        },
+      });
 
-    const menuTemplate = [lastWateredMenuItem, wateredButton];
+      const menuTemplate = [lastWateredMenuItem, wateredButton];
 
       contextMenu = Menu.buildFromTemplate(menuTemplate);
       tray.setContextMenu(contextMenu);

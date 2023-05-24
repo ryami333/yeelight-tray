1ba9f1bb9b6f12417ed030d6049bce435abf8cfd
Mitch Ryan <mitch@mitch-ryan.com>
2023-05-24 09:58:19 +0200

    Dont render an application menu for performance

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { writeFileSync, readFileSync, existsSync } from "fs";
 import { addDays, format, differenceInCalendarDays } from "date-fns";
 import ms from "ms";
 
+/**
+ * Performance improvement:
+ * https://www.electronjs.org/docs/latest/tutorial/performance#8-call-menusetapplicationmenunull-when-you-do-not-need-a-default-menu
+ */
+Menu.setApplicationMenu(null);
+
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
 

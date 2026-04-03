2598197dd47990860dee84004a8fed8af4cd7f48
Mitch Ryan <mitch@diesdas.digital>
2026-04-03 11:58:25 +0200

    Simplify eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,9 @@
 import eslintJs from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
-import typescriptEslintParser from "@typescript-eslint/parser";
-import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
+import typescriptEslint from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores(["dist/", "out/"]),
-  {
-    files: ["**/*.{js,jsx,ts,tsx,mjs}"],
-    languageOptions: {
-      parser: typescriptEslintParser,
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
-    plugins: { "@typescript-eslint": typescriptEslintPlugin },
-    rules: {
-      ...eslintJs.configs.recommended.rules,
-      ...typescriptEslintPlugin.configs.recommended.rules,
-    },
-  },
+  eslintJs.configs.recommended,
+  ...typescriptEslint.configs.recommended,
 ]);

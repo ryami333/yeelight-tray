d5a98917df64200e4e61e73e7cb4d043f2198f0a
Mitch Ryan <mitch@diesdas.digital>
2026-04-03 17:45:46 +0200

    Add env validation

--- forge.config.mjs
+++ forge.config.mjs
@@ -1,4 +1,23 @@
+import "dotenv/config";
+import { createEnv } from "@t3-oss/env-core";
+import z from "zod";
+
 /** @type {import('@electron-forge/shared-types').ForgeConfig} */
+export const env = createEnv({
+  server: {
+    APPLE_ID: z.email(),
+    APPLE_ID_PASSWORD: z.string().nonempty(),
+    APPLE_TEAM_ID: z.string().nonempty(),
+  },
+
+  /**
+   * What object holds the environment variables at runtime. This is usually
+   * `process.env` or `import.meta.env`.
+   */
+  // eslint-disable-next-line no-undef
+  runtimeEnv: process.env,
+});
+
 const config = {
   packagerConfig: {
     name: "Yeelight Tray",
@@ -6,9 +25,9 @@ const config = {
     icon: "./icons/icon",
     osxSign: {},
     osxNotarize: {
-      appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_ID_PASSWORD,
-      teamId: process.env.APPLE_TEAM_ID,
+      appleId: env.APPLE_ID,
+      appleIdPassword: env.APPLE_ID_PASSWORD,
+      teamId: env.APPLE_TEAM_ID,
     },
   },
   makers: [
